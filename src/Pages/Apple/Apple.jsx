65e417078341873e2b9fd7888d3003b9fc5334c2
shakib2123 <mshakibalhasan21@gmail.com>
2023-10-19 14:29:54 +0600

    relevant message on the brand page added

--- src/Pages/Apple/Apple.jsx
+++ src/Pages/Apple/Apple.jsx
@@ -11,6 +11,22 @@ const Apple = () => {
       </div>
       <div>
         <AppleAd></AppleAd>
+        <div className="alert alert-info max-w-7xl mx-auto mt-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-current shrink-0 w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>For more brands products you can go to the home page.</span>
+        </div>
         <AppleProducts></AppleProducts>
       </div>
     </div>
--- src/Pages/Sony/Sony.jsx
+++ src/Pages/Sony/Sony.jsx
@@ -10,6 +10,22 @@ const Sony = () => {
       </div>
       <div>
         <SonyAd></SonyAd>
+        <div className="alert alert-info max-w-7xl mx-auto mt-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-current shrink-0 w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>For more brands products you can go to the home page.</span>
+        </div>
         <SonyProducts></SonyProducts>
       </div>
     </div>
